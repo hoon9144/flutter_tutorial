1ae802da1565e2136b04a774a093a6c87cb46f79
hoon <rissk12@naver.com>
2020-08-02 18:03:04 +0900

    second commit

--- ex9/lib/main.dart
+++ ex9/lib/main.dart
@@ -1,113 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MaterialApp(home: FirestoreFirstDemo(),));
-
-FirestoreFirstDemoState pageState;
-
-class FirestoreFirstDemo extends StatefulWidget {
-  @override
-  FirestoreFirstDemoState createState() {
-    pageState = FirestoreFirstDemoState();
-    return pageState;
-  }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(home: FirestoreChat()));
 }
 
-class FirestoreFirstDemoState extends State<FirestoreFirstDemo> {
-  final firestoreInstance =  Firestore .instance;
-  TextEditingController textEditingController = TextEditingController();
+class FirestoreChat extends StatelessWidget {
+  final firestoreInstance = Firestore.instance;
+  final TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Firestore Chat!")),
-      body: SingleChildScrollView(
-        physics: ScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                height: 450,
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: Firestore.instance.collection("chat").snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError) return Text("Error: ${snapshot.error}");
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return CircularProgressIndicator();
-                      default:
-                        return ListView(
-                          children: snapshot.data.documents
-                              .map((DocumentSnapshot document) {
-                            Timestamp tt = document["datetime"];
-                            DateTime dt = DateTime.fromMicrosecondsSinceEpoch(
-                                tt.microsecondsSinceEpoch);
-                            return Card(
-                              elevation: 2,
-                              child: Container(
-                                padding: const EdgeInsets.all(8),
-                                child: Column(
-                                  children: <Widget>[
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Text(
-                                          document["name"],
-                                          style: TextStyle(
-                                            color: Colors.blueGrey,
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        Text(
-                                          dt.toString(),
-                                          style: TextStyle(color: Colors.grey[600]),
-                                        )
-                                      ],
-                                    ),
-                                    Container(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text(
-                                        document["description"],
-                                        style: TextStyle(color: Colors.black54),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                        );
-                    }
-                  },
-                ),
-              ),
-              TextField(
-                controller: textEditingController,
-                cursorWidth: 1,
-                showCursor: true,
-                decoration: InputDecoration(
-                  hintText: "여기에 입력해주세요!",
-                    suffixIcon: IconButton(icon: Icon(Icons.email), onPressed: (){
-                    print('hello?');
-                    var now = new DateTime.now();
-                    print(now);
-                    firestoreInstance.collection("chat").add(
-                        {
-                          "name" : "john",
-                          "description" : textEditingController.text,
-                          "datetime" : now
-                        }).then((value){
-                      print(value.documentID);
-                    });
-                  })
-                ),
-              )
-            ],
-          ),
-      ),
-      );
+        appBar: AppBar(title: Text("Firestore Chat!")),
+        body: Column(
+          children: <Widget>[
+            Expanded(child: msgBuild()),
+            sendAction(),
+          ],
+        ));
+  }
+
+  Widget msgBuild() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection("chat").snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Error: ${snapshot.error}");
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          default:
+            return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+//                  final m = snapshot.data.documents => List<DocumentsSnapshot>
+//                  final m = snapshot.data.documents[index] => DocumentSnapshot
+//                  final m = snapshot.data.documents[index].data; => Map<String, dynamic>
+                  final m = snapshot.data.documents[index].data;
+                 Chat  c =  Chat.fromJson(m);
+                 Timestamp date_time = c.datetime;
+                 var date = date_time.toDate();
+                 return MessageWidget(snapshots: c, date: date);
+                });
+        }
+      },
+    );
+  }
+
+  Widget sendAction() {
+    return TextField(
+      controller: textEditingController,
+      cursorWidth: 1,
+      showCursor: true,
+      decoration: InputDecoration(
+          hintText: "여기에 입력해주세요!",
+          suffixIcon: IconButton(
+              icon: Icon(Icons.email),
+              onPressed: () {
+                var now = new DateTime.now();
+                firestoreInstance.collection("chat").add({
+                  "name": "bing",
+                  "message": textEditingController.text,
+                  "datetime": now
+                });
+              })),
+    );
   }
 }
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({
+    Key key,
+    @required this.snapshots,
+    @required this.date,
+  }) : super(key: key);
+
+  final Chat snapshots;
+  final DateTime date;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(snapshots.name),
+                Spacer(),
+                Text('${date}')
+              ],
+            ),
+            SizedBox(height: 10),
+            Text('message : ${snapshots.message}')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Chat{
+  String name;
+  String message;
+  Timestamp datetime;
+
+  Chat({this.name , this.message, this.datetime});
+
+  factory Chat.fromJson(Map<String, dynamic> data){
+    return Chat(
+      name : data['name'],
+        message: data['message'],
+      datetime: data['datetime']
+    );
+  }
+}
+
